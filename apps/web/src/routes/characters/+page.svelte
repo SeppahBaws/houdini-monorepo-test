26ded187bcf99da10efdc9632eae8e4d2cfcbd88
Seppe Dekeyser <seppe@cloudclass.be>
2025-07-22 16:20:11 +0200

    Attempt at shared fragment

---
+++ apps/web/src/routes/characters/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageProps } from './$houdini';
+
+	let { data }: PageProps = $props();
+	let { AllCharacters } = $derived(data);
+</script>
+
+<h1>List of all the characters:</h1>
+{#if $AllCharacters.data}
+	{#each $AllCharacters.data.characters?.results ?? [] as character}
+		{#if character}
+			<p><a href="/{character?.id}">{character?.name}</a></p>
+		{/if}
+	{/each}
+{/if}
---
+++ apps/web/src/routes/locations/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { PageProps } from './$houdini';
+
+	let { data }: PageProps = $props();
+	let { LocationsList } = $derived(data);
+</script>
+
+{#if $LocationsList.data}
+	<ul>
+		{#each $LocationsList.data.locations?.results ?? [] as location}
+			{#if location}
+				<li>{location.name} ({location.type}) - dimension: {location.dimension}</li>
+			{/if}
+		{/each}
+	</ul>
+{/if}
